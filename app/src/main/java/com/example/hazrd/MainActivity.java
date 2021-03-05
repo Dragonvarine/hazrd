9a5069aa1921afeb13c68c5ab6f4227ca3e3904f
Ciaran May-Townsend <maytownsendciaran@gmail.com>
2021-03-05 12:48:42 +0000

    Firebase SDK

--- app/src/main/java/com/example/hazrd/MainActivity.java
+++ app/src/main/java/com/example/hazrd/MainActivity.java
@@ -1,16 +1,41 @@
 package com.example.hazrd;
 
+import java.util.Scanner;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    //Hello, world.
-}
+        mAuth = FirebaseAuth.getInstance();
+
+        Scanner scanner = new Scanner(System.in);
+        String email = scanner.nextLine();
+
+        String password = "empty";
+
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        
+                    }
+                });
+        }
+    }
