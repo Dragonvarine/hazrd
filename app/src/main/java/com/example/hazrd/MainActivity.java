bb95d21acde1ec41183432999b8d25199ea7fe59
Ciaran May-Townsend <maytownsendciaran@gmail.com>
2021-03-08 21:12:24 +0000

    Added in a very basic (and messy) log-in system.
    The Android Studio logging system "LogCat" shows that the log in is successful upon entering the right credentials. And failure upon wrong credentials.

--- app/src/main/java/com/example/hazrd/MainActivity.java
+++ app/src/main/java/com/example/hazrd/MainActivity.java
@@ -6,15 +6,27 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "?";
+    private String email;
+    private String password;
     private FirebaseAuth mAuth;
+    private EditText emailInput;
+    private EditText passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +35,57 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        Scanner scanner = new Scanner(System.in);
-        String email = scanner.nextLine();
+        emailInput = (EditText) findViewById(R.id.emailInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
 
-        String password = "empty";
+        signInUser();
+    }
+
+    protected void signInUser() {
+        Button signInButton;
+
+        signInButton = (Button) findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                email = emailInput.getText().toString();
+                password = passwordInput.getText().toString();
+
+                signInMethod(email, password);
+            }
+        });
+    }
+
+    protected void signInMethod(String email, String password)
+    {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            //FirebaseUser user = mAuth.getCurrentUser();
+                            System.out.println("Successful");
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.d(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            System.out.println("Unsuccessful");
+                        }
+                    }
+                });
+    }
+
+
+    protected void createUser() {
+        mAuth = FirebaseAuth.getInstance();
+
+        String email = "w9101532@live.tees.ac.uk";
+
+        String password = "TestPassword123";
 
 
         mAuth.createUserWithEmailAndPassword(email, password)
@@ -34,7 +93,19 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task)
                     {
-                        
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("test1", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("test2", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
                     }
                 });
         }
