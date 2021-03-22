91b11f8c9ee2b8a0670731eb58af8f568185cec0
Ciaran May-Townsend <maytownsendciaran@gmail.com>
2021-03-22 10:09:29 +0000

    Major update with register and log in systems
    Switch between the log-in and register screens by clicking the text below the sign-up and log-in buttons.

---
+++ app/src/main/java/com/example/hazrd/RegisterUser.java
@@ -0,0 +1,88 @@
+package com.example.hazrd;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterUser extends AppCompatActivity {
+
+    private static final String TAG = "Register";
+    private String email;
+    private String password;
+    private FirebaseAuth mAuth;
+    private EditText emailInput;
+    private EditText passwordInput1;
+    private EditText passwordInput2;
+    private TextView signInScreenText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emailInput = (EditText) findViewById(R.id.signUpEmailInput1);
+        passwordInput1 = (EditText) findViewById(R.id.signUpPasswordInput1);
+        passwordInput2 = (EditText) findViewById(R.id.signUpPasswordInput2);
+
+        signInScreenText = (TextView) findViewById(R.id.alreadyUserButton);
+
+        //registerUser();
+        navigateToLogin();
+    }
+
+    private void registerUser() {
+        mAuth = FirebaseAuth.getInstance();
+
+        email = emailInput.getText().toString();
+
+        //If both password inputs are equal
+        if (passwordInput1.getText().toString().equals(passwordInput2.getText().toString())) {
+            password = passwordInput1.getText().toString();
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.d(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterUser.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        Intent intent = new Intent(RegisterUser.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+    }
+
+    private void navigateToLogin() {
+        signInScreenText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterUser.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
